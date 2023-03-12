8198a2ba3ae914ab61637a4bd3859ec8d92365e4
girogiosld <giorgiosaldana99@gmail.com>
2023-03-12 13:16:51 +0100

    First function for bot Telegram

--- bot/auth_telegram.py
+++ bot/auth_telegram.py
@@ -1,6 +1,7 @@
 from telethon import TelegramClient
+from telethon import functions
 
-
+#function to login in tg account
 def  __auth():
     api_id = 123456789 # Replace this with your own api_id
     api_hash = '' # Replace this with your own api_hash
@@ -12,3 +13,13 @@ def  __auth():
         client.send_code_request(phone)
         client.sign_in(phone, input('Enter the code: ')) 
     return client
+
+#function to send message from bot
+def __core_to_send_message(client, receiver, msg):
+    client((functions.messages.SendMessageRequest(
+        peer=receiver,
+        message=msg,
+        silent=False,
+    )))
+    
+
